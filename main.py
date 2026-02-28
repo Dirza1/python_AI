bbb92dbc1341b83c0b51dae7565c42143763af1d
Dirza1 <jasper.olthof@xs4all.nl>
2026-02-28 21:05:21 +0100

    created model, API key and calculator app

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import os
+import argparse
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
 def main():
-    print("Hello from python-ai!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key or api_key == None:
+        raise RuntimeError("API key was not found")
+    
+    parser:argparse.ArgumentParser = argparse.ArgumentParser(description="Python chatbot")
+    parser.add_argument("user_prompt",type=str,help="The prompt the user supplied into the program")
+    parser.add_argument("--verbose",action="store_true",help="enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    client:genai.Client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(model="gemini-2.5-flash",contents=messages)
+    if not response.usage_metadata or response.usage_metadata == None:
+        raise RuntimeError("Likely failed API request. Please try again")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
 
 
 if __name__ == "__main__":
